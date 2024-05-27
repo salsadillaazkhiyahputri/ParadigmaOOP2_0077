2ac8ea8516620d8648e9a88a53d4f583e36284ad
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-27 15:14:34 +0700

    input class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -13,3 +13,13 @@ public:
     
 };
     
+class pekerja : virtual public orang {
+    public:
+
+    pekerja(int pUmur):
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
