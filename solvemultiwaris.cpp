95e48abee4582261abced80a66a841dce34801a3
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-27 15:24:37 +0700

    solvemultiwaris revisi

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -23,3 +23,29 @@ class pekerja : public orang {
     }
 };
 
+class pelajar : public orang {
+    public:
+
+    pelajar(int pUmur):
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class  budi : public pekerja, public pelajar{
+    public:
+
+    budi(int pUmur):
+    pekerja(pUmur),
+    pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main(){
+    budi a(12);
+
+    return 0;
+}
