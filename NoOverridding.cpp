95917d6a6eaf8db80be598824ea97b1a1c3e0b98
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-27 14:52:48 +0700

    input class derivedClass

--- NoOverridding.cpp
+++ NoOverridding.cpp
@@ -8,3 +8,10 @@ class baseClass{
     }
 };
 
+class derivedClass : public baseClass{
+    public:
+    void perkenalan(){        
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
